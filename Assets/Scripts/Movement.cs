9643350c1c6cfc7e11c76f3e6a392bad0cf481c7
serlock <serkanakyuz72@gmail.com>
2022-09-09 16:06:56 +0300

    Obstackle is moving

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,28 +5,50 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public Transform obstackle;
+    public Transform point;
     Vector3 position;
+    string mothodName = "";
 
     // This part is not done yet
     void Update()
     {
+        if (obstackle.transform.position.x >= -5.5 && obstackle.transform.position.x <= -5)
+        {
+            mothodName = "MoveRight";
+        }
+
+        if (obstackle.transform.position.x > 5 && obstackle.transform.position.x <= 5.5)
+        {
+            mothodName = "MoveLeft";
+        }
+
+        Invoke(mothodName, 0.01f);
+    }
+
+    void MoveLeft()
+    {
         position = obstackle.transform.position;
-
-        if (position.x <= 5.5)
-        {
-            if(position.x >= -5)
-            {
-                position.x -= 0.05f;
-            }
-        }
-        else if (position.x >= -5.5)
-        {
-            if (position.x <= 5)
-            {
-                position.x += 0.05f;
-            }
-        }
-
+        position.x -= 0.01f;
         obstackle.transform.position = position;
+        position.y = 3.5f;
+
+        if (point)
+        {
+            point.transform.position = position;
+        }
+    }
+
+    void MoveRight()
+    {
+        position = obstackle.transform.position;
+        position.x += 0.01f;
+        obstackle.transform.position = position;
+        position.y = 3.5f;
+
+        if (point)
+        {
+            point.transform.position = position;
+        }
+
     }
 }
